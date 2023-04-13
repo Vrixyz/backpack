36b533302561fd597faf3653d01b17ab11c9494f
Thierry Berger <contact@thierryberger.com>
2023-04-13 18:24:33 +0200

    fix build script for wasm

--- crates/example_game_lazy/scripts/build_wasm.sh
+++ crates/example_game_lazy/scripts/build_wasm.sh
@@ -6,6 +6,8 @@ set -eo pipefail
 BINARY_NAME=example_game_lazy
 
 cargo build --bin $BINARY_NAME --profile wasm-release --target wasm32-unknown-unknown
-cp -r wasm generated_wasm
-wasm-bindgen --no-typescript --out-name bevy_game --out-dir generated_wasm --target web ../../target/wasm32-unknown-unknown/wasm-release/$BINARY_NAME.wasm
-cp -r assets generated_wasm
+cp -r wasm generated_wasm_new
+wasm-bindgen --no-typescript --out-name bevy_game --out-dir generated_wasm_new --target web ../../target/wasm32-unknown-unknown/wasm-release/$BINARY_NAME.wasm
+cp -r assets generated_wasm_new
+rm -rf generated_wasm
+mv generated_wasm_new generated_wasm
